b490449befe603d388baa4a184c7130637dfffd6
scriptden <77801991+scriptden@users.noreply.github.com>
2021-10-24 15:54:48 +0200

    Create urllinks.py

---
+++ urllinks.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.parse, urllib
+from bs4 import BeautifulSoup
+import ssl
+
+#Ignore SSL cetificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter - ")
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html,  "html.parser")
+
+#Retrive all of the anchot tags
+tags = soup("a")
+for tag in tags:
+    print(tag.get("href", None))
