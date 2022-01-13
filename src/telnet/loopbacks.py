23101752334b952ef6f1192493db40038f17af8a
Wanzaz <ondra.pazourek@gmail.com>
2022-01-13 12:36:20 +0100

    dir telnet + loopbacks.py created

---
+++ src/telnet/loopbacks.py
@@ -0,0 +1,37 @@
+#username david password cisco
+#line vty 0 4
+#login local
+#transport input all
+#telnet 192.168.122.71
+#enable password cisco
+#debug telnet
+
+import getpass
+import telnetlib
+
+HOST = "localhost"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"login: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+tn.write(b"exit\n")
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"int loop 1\n")
+tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
+tn.write(b"router ospf 1\n")
+tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"end")
+tn.write(b"exit")
+
+print(tn.read_all().decode('ascii'))
+
